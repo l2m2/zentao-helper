e81acb799347b347f48491a78480f975b3da056d
l2m2lq@gmail.com <114723704@qq.com>
2019-02-22 11:39:27 +0800

    upload.

---
+++ zentaohelper/__init__.py
@@ -0,0 +1,5 @@
+'''
+@File: __init__.py
+@Author: leon.li(l2m2lq@gmail.com)
+@Date: 2019-02-22 11:34:48
+'''
---
+++ zentaohelper/zentaohelper.py
@@ -0,0 +1,22 @@
+'''
+@File: zentaohelper.py
+@Author: leon.li(l2m2lq@gmail.com)
+@Date: 2019-02-22 11:34:40
+'''
+
+class ZentaoHelper:
+  """
+  禅道助手
+  """
+
+  def __init__(self):
+    pass
+
+  def login(self):
+    pass
+
+  def createBug(self):
+    pass
+
+  def createSprintBuild(self):
+    pass
